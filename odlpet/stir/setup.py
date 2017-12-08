e89f61f94e3b39fb49ee40b6184e480d2f660256
Olivier Verdier <olivier.verdier@gmail.com>
2017-12-08 17:10:15 +0100

    move scanner creation function in scanner module

--- odlpet/stir/setup.py
+++ odlpet/stir/setup.py
@@ -173,45 +173,6 @@ def stir_get_ODL_geometry_which_honours_STIR_restrictions(_det_y_size_mm, _det_z
                                                 axialpart)
 
 
-def stir_get_STIR_geometry(_num_rings, _num_dets_per_ring,
-                           _det_radius, _ring_spacing,
-                           _average_depth_of_inter,
-                           _voxel_size_xy,
-                           _axial_crystals_per_block = 1, _trans_crystals_per_block= 1,
-                           _axials_blocks_per_bucket = 1, _trans_blocks_per_bucket = 1,
-                           _axial_crystals_per_singles_unit = 1, _trans_crystals_per_singles_unit = 1,
-                           _num_detector_layers = 1, _intrinsic_tilt = 0):
-
-    # Roughly speaking number of detectors on the diameter
-    # bin_size = (_det_radius*2) / (_num_dets_per_ring/2)
-    max_num_non_arc_cor_bins = int(_num_dets_per_ring/2)
-
-    # TODO: use "Userdefined" instead? (should not change much)
-    scanner = stir.Scanner.get_scanner_from_name('')
-
-    scanner.set_num_rings(np.int32(_num_rings))
-    scanner.set_num_detectors_per_ring(np.int32(_num_dets_per_ring))
-    scanner.set_default_bin_size(np.float32(_voxel_size_xy))
-    scanner.set_default_num_arccorrected_bins(np.int32(max_num_non_arc_cor_bins))
-    scanner.set_default_intrinsic_tilt(np.float32(_intrinsic_tilt))
-    scanner.set_inner_ring_radius(np.float32(_det_radius))
-    scanner.set_ring_spacing(np.float32(_ring_spacing))
-    scanner.set_average_depth_of_interaction(np.float32(_average_depth_of_inter))
-    scanner.set_max_num_non_arccorrected_bins(np.int32(max_num_non_arc_cor_bins))
-    scanner.set_num_axial_blocks_per_bucket(np.int32(_axials_blocks_per_bucket))
-    scanner.set_num_transaxial_blocks_per_bucket(np.int32(_trans_blocks_per_bucket))
-    scanner.set_num_axial_crystals_per_block(np.int32(_axial_crystals_per_block))
-    scanner.set_num_transaxial_crystals_per_block(np.int32(_trans_crystals_per_block))
-    scanner.set_num_axial_crystals_per_singles_unit(np.int32(_axial_crystals_per_singles_unit))
-    scanner.set_num_transaxial_crystals_per_singles_unit(np.int32(_trans_crystals_per_singles_unit))
-    scanner.set_num_detector_layers(np.int32(_num_detector_layers))
-
-    # TODO: this does not work. check_consistency does not return a Boolean
-    # should be success == stir.Succeeded(stir.Succeeded.yes)
-    if scanner.check_consistency():
-        return scanner
-    else:
-        raise TypeError('Something is wrong in the scanner geometry.')
 
 
 def stir_get_projection_data_info(_stir_scanner, _span_num,
