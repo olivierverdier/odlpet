0b2e1babb580878001ade1dd8572e48f966294d9
Olivier Verdier <olivier.verdier@gmail.com>
2018-02-02 15:28:24 +0100

    simplify test_memory_run

--- test/test_run.py
+++ test/test_run.py
@@ -2,11 +2,6 @@ from os import path
 import odl
 
 
-from odlpet.stir.setup import (
-    stir_get_ODL_domain_which_honours_STIR_restrictions,
-    stir_get_STIR_domain_from_ODL,
-    stir_get_ODL_domain_from_STIR,
-)
 
 from odlpet.stir.bindings import stir_projector_from_memory, stir_projector_from_file
 from odlpet.scanner.scanner import mCT
@@ -16,12 +11,9 @@ def test_memory_run():
     """
     Check that a simple setup runs without errors.
     """
-    discr_dom_odl = stir_get_ODL_domain_which_honours_STIR_restrictions([2**6, 2**6, 15], [2.05941, 2.05941, 3.125])
-    stir_domain = stir_get_STIR_domain_from_ODL(discr_dom_odl, 0.0)
     scanner = mCT()
     compression = Compression(scanner)
-    dummy_discr_dom_odl = stir_get_ODL_domain_from_STIR(stir_domain)
-    proj = stir_projector_from_memory(dummy_discr_dom_odl, stir_domain, compression)
+    proj = compression.get_projector(domain=compression.get_domain(zoom=1))
     result = proj(proj.domain.one())
 
 def test_file_run():
