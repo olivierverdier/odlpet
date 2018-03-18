15fffb1df147b6f61427138c35aef2b93cef7fd3
Olivier Verdier <olivier.verdier@gmail.com>
2018-03-18 13:48:36 +0100

    ENH: add minimal setup file

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: UTF-8
+
+
+from distutils.core import setup
+
+setup(
+    name         = 'odlpet',
+    version      = '0.1',
+    description  = 'ODL Pet',
+    author = 'Olivier Verdier',
+    packages=['odlpet',],
+    classifiers = [
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    )
