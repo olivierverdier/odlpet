4184a941c26d4eaffe2456cf892b9f26f4d1359d
Olivier Verdier <olivier.verdier@gmail.com>
2018-04-13 18:12:44 +0200

    ENH: io convenience function to get proj_data_info from sinogram header
    not tested yet

--- odlpet/stir/io.py
+++ odlpet/stir/io.py
@@ -20,14 +20,22 @@ def space_from_file(volume_file):
     _voxels = stir_domain_from_file(volume_file)
     return space_from_stir_domain(_voxels)
 
+def _proj_data_info_from_file(projection_file):
+    """
+    Convenience function to create a stir proj_data_info object from
+    a projection header file.
+    """
+    proj_data = _ProjData.read_from_file(projection_file)
+    proj_data_info = proj_data.get_proj_data_info()
+    return proj_data_info
+
 def projector_from_file(volume_file, projection_file):
     """
     Convenience function to create a projector from a volume and projection
     header files.
     """
     stir_domain = stir_domain_from_file(volume_file)
-    proj_data = _ProjData.read_from_file(projection_file)
-    proj_data_info = proj_data.get_proj_data_info()
+    proj_data_info = _proj_data_info_from_file(projection_file)
     comp = Compression.from_stir_proj_data_info(proj_data_info)
     proj = comp.get_projector(stir_domain=stir_domain,
                        stir_proj_data_info=proj_data_info)
