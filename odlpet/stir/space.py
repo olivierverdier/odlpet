556e61e464fbe6cc76bfec6c97cdc9c68374a0aa
Olivier Verdier <olivier.verdier@gmail.com>
2018-04-13 12:21:01 +0200

    MAINT: more consistent naming

--- odlpet/stir/space.py
+++ odlpet/stir/space.py
@@ -1,29 +1,29 @@
 from odl.discr import uniform_discr
 
-def space_from_stir_domain(_voxels):
+def space_from_stir_domain(stir_domain):
     """
     This function tries to transform the VoxelsOnCartesianGrid to
     DicreteLP.
 
     Parameters
     ----------
-    _voxels: A VoxelsOnCartesianGrid Object
+    stir_domain: A VoxelsOnCartesianGrid Object
 
     Returns
     -------
     An ODL DiscreteLP object with characteristics of the VoxelsOnCartesianGrid
     """
 
-    stir_vox_max = _voxels.get_max_indices()
-    stir_vox_min = _voxels.get_min_indices()
+    stir_vox_max = stir_domain.get_max_indices()
+    stir_vox_min = stir_domain.get_min_indices()
     vox_num = [stir_vox_max[1] - stir_vox_min[1] +1,
                stir_vox_max[2] - stir_vox_min[2] +1,
                stir_vox_max[3] - stir_vox_min[3] +1]
 
-    stir_vol_max = _voxels.get_physical_coordinates_for_indices(_voxels.get_max_indices())
-    stir_vol_min = _voxels.get_physical_coordinates_for_indices(_voxels.get_min_indices())
+    stir_vol_max = stir_domain.get_physical_coordinates_for_indices(stir_domain.get_max_indices())
+    stir_vol_min = stir_domain.get_physical_coordinates_for_indices(stir_domain.get_min_indices())
 
-    stir_vox_size = _voxels.get_voxel_size()
+    stir_vox_size = stir_domain.get_voxel_size()
 
     vol_max = [stir_vol_max[1]+stir_vox_size[1], stir_vol_max[2]+stir_vox_size[2],stir_vol_max[3]+stir_vox_size[3]]
     vol_min = [stir_vol_min[1], stir_vol_min[2],stir_vol_min[3]]
